bac69d66ba30967fc7ae23d241d0ee85f9a1aeda
sakuma <sakuma@sakumanoMacBook-Pro.local>
2019-06-03 20:04:16 +0900

    first commit

--- main.py
+++ main.py
@@ -236,12 +236,18 @@ def main():
 
                 network_optimizer.step()
 
+                print(list(network.parameters())[0].grad)
+
                 network_optimizer.zero_grad()
                 architecture_optimizer.zero_grad()
+
+                print(list(network.parameters())[0].grad)
 
                 val_logits = model(val_images)
                 val_loss = criterion(val_logits, val_labels)
                 val_loss.backward()
+
+                print(list(network.parameters())[0].grad)
 
                 new_network_parameters = [parameter.clone() for parameter in network.parameters()]
                 norm = torch.norm(torch.cat([parameter.grad.reshape(-1) for parameter in new_network_parameters]))
