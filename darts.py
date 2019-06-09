f1e10fa0ed8f9df7b7d3a276a0537deb6ebac9f1
sakuma <sakuma@sakumanoMacBook-Pro.local>
2019-06-09 16:45:44 +0900

    update

--- darts.py
+++ darts.py
@@ -1,10 +1,10 @@
 import torch
 from torch import nn
 from ops import *
+from utils import *
 from operator import *
 import networkx as nx
 import graphviz as gv
-import matplotlib.pyplot as plt
 
 
 class DARTS(nn.Module):
@@ -16,16 +16,19 @@ class DARTS(nn.Module):
 
     """
 
-    def __init__(self, operations, num_nodes, num_input_nodes, num_cells, reduction_cells, num_channels, num_classes):
+    def __init__(self, operations, num_nodes, num_input_nodes, num_cells, reduction_cells,
+                 discrete_mode, num_top_operations, num_channels, num_classes):
         """Build DARTS with the given operations.
 
         Args:
-            operations (dict): Dict with name as keys and nn.Module initializer 
+            operations (dict): Dict with name as keys and nn.Module initializer
                 that takes in_channels, out_channels, stride as arguments as values.
             num_nodes (int): Number of nodes in each cell.
             num_input_nodes (int): Number of input nodes in each cell.
             num_cells (int): Number of cells in the network.
             reduction_cells (list): List of cell index that performs spatial reduction.
+            discrete_mode (bool): Whether use discrete architecture.
+            num_top_operations (int): Number of top strongest operations retained in discrete architecture.
             num_channels (int): Number of channels of the first cell.
             num_classes (int): Number of classes for classification.
 
@@ -37,40 +40,77 @@ class DARTS(nn.Module):
         self.num_input_nodes = num_input_nodes
         self.num_cells = num_cells
         self.reduction_cells = reduction_cells
+        self.discrete_mode = discrete_mode
+        self.num_top_operations = num_top_operations
         self.num_channels = num_channels
         self.num_classes = num_classes
 
-        self.build_dag()
-        self.build_architecture()
-        self.build_network()
+        self.dag = Dict()
+        self.dag.normal = nx.DiGraph()
+        self.dag.reduction = nx.DiGraph()
+        self.architecture = nn.ParameterDict()
+        self.architecture.normal = nn.ParameterDict()
+        self.architecture.reduction = nn.ParameterDict()
+        self.network = nn.ModuleDict()
 
-    def build_dag(self):
+        if self.discrete_mode:
+            self.build_dag()
+            self.build_architecture()
+            self.build_discrete_dag()
+            self.build_discrete_network()
+        else:
+            self.build_dag()
+            self.build_architecture()
+            self.build_network()
+
+    def build_dag(self, reduction=None):
         """Build Directed Acyclic Graph that represents each cell.
         """
-        self.dag = nx.DiGraph()
-        for parent in range(self.num_nodes):
-            for child in range(self.num_input_nodes, self.num_nodes):
-                if parent < child:
-                    self.dag.add_edge(parent, child)
+        if reduction is None:
+            self.build_dag(reduction=True)
+            self.build_dag(reduction=False)
 
-    def build_architecture(self):
+        dag = self.dag.reduction if reduction else self.dag.normal
+        architecture = self.architecture.reduction if reduction else self.architecture.normal
+
+        for m in range(self.num_nodes):
+            for n in range(self.num_input_nodes, self.num_nodes):
+                if m < n:
+                    dag.add_edge(m, n)
+
+    def build_discrete_dag(self, reduction=None):
+        """Build Directed Acyclic Graph that represents each cell.
+        """
+        if reduction is None:
+            self.build_discrete_dag(reduction=True)
+            self.build_discrete_dag(reduction=False)
+
+        dag = self.dag.reduction if reduction else self.dag.normal
+        architecture = self.architecture.reduction if reduction else self.architecture.normal
+
+        for n in dag.nodes():
+            operations = sorted(((*max(zip(
+                nn.functional.softmax(architecture[str((m, n))], dim=0),
+                self.operations.keys()
+            ), key=itemgetter(0)), m) for m in dag.predecessors(n)), key=itemgetter(0))
+            for weight, operation, m in operations[:-self.num_top_operations]:
+                dag.remove_edge(m, n)
+
+    def build_architecture(self, reduction=None):
         """Build parameters that represent the cell architectures (normal and reduction).
         """
-        self.architecture = nn.ParameterDict()
-        self.architecture.normal = nn.ParameterDict({
-            str((parent, child)): nn.Parameter(torch.zeros(len(self.operations)))
-            for parent, child in self.dag.edges()
-        })
-        self.architecture.reduction = nn.ParameterDict({
-            str((parent, child)): nn.Parameter(torch.zeros(len(self.operations)))
-            for parent, child in self.dag.edges()
-        })
+        if reduction is None:
+            self.build_architecture(reduction=True)
+            self.build_architecture(reduction=False)
+
+        dag = self.dag.reduction if reduction else self.dag.normal
+        architecture = self.architecture.reduction if reduction else self.architecture.normal
+
+        architecture.update({str((m, n)): nn.Parameter(torch.zeros(len(self.operations))) for m, n in dag.edges()})
 
     def build_network(self):
         """Build modules that represent the whole network.
         """
-        self.network = nn.ModuleDict()
-
         # NOTE: Why multiplier is 3?
         num_channels = self.num_channels
         out_channels = num_channels * 3
@@ -95,26 +135,29 @@ class DARTS(nn.Module):
                 reduction = True
                 num_channels <<= 1
 
+            dag = self.dag.reduction if reduction else self.dag.normal
+            architecture = self.architecture.reduction if reduction else self.architecture.normal
+
             cell = nn.ModuleDict({
-                **{
-                    str((parent, child)): nn.ModuleList([
-                        operation(
-                            in_channels=num_channels,
-                            out_channels=num_channels,
-                            stride=2 if reduction and parent in range(self.num_input_nodes) else 1
-                        ) for operation in self.operations.values()
-                    ]) for parent, child in self.dag.edges()
-                },
                 # NOTE: Should be factorized reduce?
                 **{
-                    str((input_node - self.num_input_nodes, input_node)): Conv2d(
-                        in_channels=out_channels[input_node - self.num_input_nodes],
+                    str((n - self.num_input_nodes, n)): Conv2d(
+                        in_channels=out_channels[n - self.num_input_nodes],
                         out_channels=num_channels,
                         stride=1 << len([j for j in self.reduction_cells if k < j < i]),
                         kernel_size=1,
                         padding=0,
                         affine=False
-                    ) for input_node, k in zip(range(0, self.num_input_nodes), range(i - self.num_input_nodes, i))
+                    ) for n, k in zip(range(self.num_input_nodes), range(i - self.num_input_nodes, i))
+                },
+                **{
+                    str((m, n)): nn.ModuleList([
+                        operation(
+                            in_channels=num_channels,
+                            out_channels=num_channels,
+                            stride=2 if reduction and m in range(self.num_input_nodes) else 1
+                        ) for operation in self.operations.values()
+                    ]) for m, n in dag.edges()
                 }
             })
 
@@ -124,69 +167,130 @@ class DARTS(nn.Module):
         self.network.global_avg_pool2d = nn.AdaptiveAvgPool2d(1)
         self.network.linear = nn.Linear(out_channels[-1], self.num_classes, bias=True)
 
-    def forward_cell(self, cell, reduction, child, node_outputs):
+    def build_discrete_network(self):
+        """Build modules that represent the whole network.
+        """
+        # NOTE: Why multiplier is 3?
+        num_channels = self.num_channels
+        out_channels = num_channels * 3
+
+        self.network.conv = Conv2d(
+            in_channels=3,
+            out_channels=out_channels,
+            stride=1,
+            kernel_size=3,
+            padding=1,
+            affine=True,
+            preactivation=False
+        )
+
+        out_channels = [out_channels] * self.num_input_nodes
+        self.network.cells = nn.ModuleList()
+
+        for i in range(self.num_cells):
+
+            reduction = False
+            if i in self.reduction_cells:
+                reduction = True
+                num_channels <<= 1
+
+            dag = self.dag.reduction if reduction else self.dag.normal
+            architecture = self.architecture.reduction if reduction else self.architecture.normal
+
+            cell = nn.ModuleDict({
+                # NOTE: Should be factorized reduce?
+                **{
+                    str((n - self.num_input_nodes, n)): Conv2d(
+                        in_channels=out_channels[n - self.num_input_nodes],
+                        out_channels=num_channels,
+                        stride=1 << len([j for j in self.reduction_cells if k < j < i]),
+                        kernel_size=1,
+                        padding=0,
+                        affine=False
+                    ) for n, k in zip(range(self.num_input_nodes), range(i - self.num_input_nodes, i))
+                },
+                **{
+                    str((m, n)): max(((weight, operation) for weight, (name, operation) in zip(
+                        nn.functional.softmax(architecture[str((m, n))], dim=0),
+                        self.operations.items()
+                    ) if 'zero' not in name), key=itemgetter(0))[1](
+                        in_channels=num_channels,
+                        out_channels=num_channels,
+                        stride=2 if reduction and m in range(self.num_input_nodes) else 1
+                    ) for m, n in dag.edges()
+                }
+            })
+
+            out_channels.append(num_channels * (self.num_nodes - self.num_input_nodes))
+            self.network.cells.append(cell)
+
+        self.network.global_avg_pool2d = nn.AdaptiveAvgPool2d(1)
+        self.network.linear = nn.Linear(out_channels[-1], self.num_classes, bias=True)
+
+    def forward_cell(self, cell, reduction, cell_outputs, node_outputs, n):
         """forward in the given cell.
 
         Args:
             cell (dict): A dict with edges as keys and operations as values.
             reduction (bool): Whether the cell performs spatial reduction.
-            child (int): The output node in the cell.
             node_outputs (dict): A dict with node as keys and its outputs as values.
                 This is to avoid duplicate calculation in recursion.
+            n (int): The output node in the cell.
 
         """
+        dag = self.dag.reduction if reduction else self.dag.normal
         architecture = self.architecture.reduction if reduction else self.architecture.normal
-        if self.dag.predecessors(child):
-            if child not in node_outputs:
-                node_outputs[child] = sum(sum(
-                    operation(self.forward_cell(cell, reduction, parent, node_outputs)) * weight
-                    for operation, weight in zip(cell[str((parent, child))], nn.functional.softmax(architecture[str((parent, child))], dim=0))
-                ) for parent in self.dag.predecessors(child))
-        return node_outputs[child]
+
+        if n not in node_outputs:
+            if n in range(self.num_input_nodes):
+                node_outputs[n] = cell[str((n - self.num_input_nodes, n))](cell_outputs[n - self.num_input_nodes])
+            else:
+                if self.discrete_mode:
+                    node_outputs[n] = sum(
+                        cell[str((m, n))](self.forward_cell(cell, reduction, cell_outputs, node_outputs, m))
+                        for m in dag.predecessors(n)
+                    )
+                else:
+                    node_outputs[n] = sum(sum(
+                        weight * operation(self.forward_cell(cell, reduction, cell_outputs, node_outputs, m))
+                        for weight, operation in zip(nn.functional.softmax(architecture[str((m, n))], dim=0), cell[str((m, n))])
+                    ) for m in dag.predecessors(n))
+        return node_outputs[n]
 
     def forward(self, input):
         output = self.network.conv(input)
         cell_outputs = [output] * self.num_input_nodes
         for i, cell in enumerate(self.network.cells):
-            node_outputs = {
-                input_node: cell[str((input_node - self.num_input_nodes, input_node))](cell_outputs[input_node - self.num_input_nodes])
-                for input_node in range(self.num_input_nodes)
-            }
-            self.forward_cell(cell, i in self.reduction_cells, self.num_nodes - 1, node_outputs)
-            _, node_outputs = zip(*sorted(node_outputs.items()))
-            cell_outputs.append(torch.cat(node_outputs[self.num_input_nodes:], dim=1))
+            node_outputs = {}
+            cell_outputs.append(torch.cat([
+                self.forward_cell(cell, i in self.reduction_cells, cell_outputs, node_outputs, n)
+                for n in range(self.num_input_nodes, self.num_nodes)
+            ], dim=1))
         output = cell_outputs[-1]
         output = self.network.global_avg_pool2d(output).squeeze()
         output = self.network.linear(output)
         return output
 
-    def draw_architecture(self, archirecture, num_operations, name, directory):
+    def render_discrete_architecture(self, reduction, name, directory):
         """Render the given architecture.
 
-        Args: 
+        Args:
             architecture (dict): A dict with edges as keys and parameters as values.
-            num_operations (int): Retain the top-k strongest operations from distinct nodes.
             name (str): Name of the given architecture for saving.
             directory (str): Directory for saving.
 
         """
-        dag = gv.Digraph(name)
-        for child in self.dag.nodes():
-            edges = [(
-                tuple(map(str, (parent, child))),
-                max((weight, operation) for weight, operation in zip(
-                    nn.functional.softmax(archirecture[str((parent, child))], dim=0),
-                    self.operations.keys()
-                ) if 'zero' not in operation)
-            ) for parent in self.dag.predecessors(child)]
-            if edges:
-                edges = sorted(edges, key=itemgetter(1))[-num_operations:]
-                for (parent, child), (weight, operation) in edges:
-                    dag.edge(parent, child, label=operation)
-        return dag.render(directory=directory, format='png')
+        dag = self.dag.reduction if reduction else self.dag.normal
+        architecture = self.architecture.reduction if reduction else self.architecture.normal
 
-    def draw_normal_architecture(self, num_operations, name, directory):
-        return self.draw_architecture(self.architecture.normal, num_operations, name, directory)
-
-    def draw_reduction_architecture(self, num_operations, name, directory):
-        return self.draw_architecture(self.architecture.reduction, num_operations, name, directory)
+        discrete_dag = gv.Digraph(name)
+        for n in dag.nodes():
+            operations = sorted(((*max(zip(
+                nn.functional.softmax(architecture[str((m, n))], dim=0),
+                self.operations.keys()
+            ), key=itemgetter(0)), m) for m in dag.predecessors(n)), key=itemgetter(0))
+            for weight, operation, m in operations[:-self.num_top_operations]:
+                discrete_dag.edge(str(m), str(n), label='', color='white')
+            for weight, operation, m in operations[-self.num_top_operations:]:
+                discrete_dag.edge(str(m), str(n), label=operation, color='black')
+        return discrete_dag.render(directory=directory, format='png')
---
+++ train.py
@@ -0,0 +1,289 @@
+import torch
+from torch import nn
+from torch import distributed
+from torch import optim
+from torch import utils
+from torch import backends
+from torch import autograd
+from torchvision import datasets
+from torchvision import transforms
+from torchvision import models
+from tensorboardX import SummaryWriter
+from darts import *
+from ops import *
+from utils import *
+import numpy as np
+import skimage
+import functools
+import argparse
+import copy
+import json
+import time
+import os
+
+
+def apply_dict(function, dictionary):
+    if isinstance(dictionary, dict):
+        for key, value in dictionary.items():
+            dictionary[key] = apply_dict(function, value)
+        dictionary = function(dictionary)
+    return dictionary
+
+
+def main(args):
+
+    backends.cudnn.benchmark = True
+
+    # python -m torch.distributed.launch --nproc_per_node=NUM_GPUS main.py
+    distributed.init_process_group(backend='mpi')
+
+    with open(args.config) as file:
+        config = apply_dict(Dict, json.load(file)).train
+    config.update(vars(args))
+    config.update(dict(
+        world_size=distributed.get_world_size(),
+        global_rank=distributed.get_rank(),
+        device_count=torch.cuda.device_count(),
+        local_rank=distributed.get_rank() % torch.cuda.device_count()
+    ))
+    print(f'config: {config}')
+
+    torch.manual_seed(0)
+    torch.cuda.set_device(config.local_rank)
+
+    model = DARTS(
+        operations=dict(
+            sep_conv_3x3=functools.partial(SeparableConv2d, kernel_size=3, padding=1, affine=True),
+            sep_conv_5x5=functools.partial(SeparableConv2d, kernel_size=5, padding=2, affine=True),
+            dil_conv_3x3=functools.partial(DilatedConv2d, kernel_size=3, padding=2, dilation=2, affine=True),
+            dil_conv_5x5=functools.partial(DilatedConv2d, kernel_size=5, padding=4, dilation=2, affine=True),
+            avg_pool_3x3=functools.partial(AvgPool2d, kernel_size=3, padding=1),
+            max_pool_3x3=functools.partial(MaxPool2d, kernel_size=3, padding=1),
+            identity=functools.partial(Conv2d, kernel_size=1, padding=0, affine=True),
+            zero=functools.partial(Zero)
+        ),
+        num_nodes=6,
+        num_input_nodes=2,
+        num_cells=20,
+        reduction_cells=[6, 13],
+        discrete_mode=True,
+        num_top_operations=2,
+        num_channels=36,
+        num_classes=10
+    ).cuda()
+
+    criterion = nn.CrossEntropyLoss(reduction='mean').cuda()
+
+    config.global_batch_size = config.local_batch_size * config.world_size
+    config.network_lr = config.network_lr * config.global_batch_size / config.global_batch_denom
+
+    network_optimizer = torch.optim.SGD(
+        params=model.network.parameters(),
+        lr=config.network_lr,
+        momentum=config.network_momentum,
+        weight_decay=config.network_weight_decay
+    )
+
+    # nn.parallel.DistributedDataParallel and apex.parallel.DistributedDataParallel don't support multiple backward passes.
+    # This means `all_reduce` is executed when the first backward pass.
+    # So, we manually reduce all gradients.
+    # model = parallel.DistributedDataParallel(model, delay_allreduce=True)
+
+    def average_gradients(parameters):
+        for parameter in parameters:
+            distributed.all_reduce(parameter.grad.data)
+            parameter.grad.data /= config.world_size
+
+    def average_tensors(tensors):
+        for tensor in tensors:
+            distributed.all_reduce(tensor.data)
+            tensor.data /= config.world_size
+
+    last_epoch = -1
+    global_step = 0
+    checkpoint = Dict(torch.load("checkpoints/search/epoch_49"))
+    model.architecture.load_state_dict(checkpoint.architecture_state_dict)
+    if config.checkpoint:
+        checkpoint = Dict(torch.load(config.checkpoint))
+        model.network.load_state_dict(checkpoint.network_state_dict)
+        model.architecture.load_state_dict(checkpoint.architecture_state_dict)
+        network_optimizer.load_state_dict(checkpoint.network_optimizer_state_dict)
+        last_epoch = checkpoint.last_epoch
+        global_step = checkpoint.global_step
+
+    if config.global_rank == 0:
+        os.makedirs(config.checkpoint_directory, exist_ok=True)
+        os.makedirs(config.event_directory, exist_ok=True)
+        summary_writer = SummaryWriter(config.event_directory)
+
+    if config.training:
+
+        lr_scheduler = optim.lr_scheduler.CosineAnnealingLR(
+            optimizer=network_optimizer,
+            T_max=config.num_epochs,
+            eta_min=config.network_lr_min,
+            last_epoch=last_epoch
+        )
+
+        train_dataset = datasets.CIFAR10(
+            root='cifar10',
+            train=True,
+            transform=transforms.Compose([
+                transforms.RandomCrop(32, padding=4),
+                transforms.RandomHorizontalFlip(),
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=(0.49139968, 0.48215827, 0.44653124),
+                    std=(0.24703233, 0.24348505, 0.26158768)
+                )
+            ]),
+            download=True
+        )
+        val_dataset = datasets.CIFAR10(
+            root='cifar10',
+            train=False,
+            transform=transforms.Compose([
+                transforms.ToTensor(),
+                transforms.Normalize(
+                    mean=(0.49139968, 0.48215827, 0.44653124),
+                    std=(0.24703233, 0.24348505, 0.26158768)
+                )
+            ]),
+            download=True
+        )
+
+        train_sampler = utils.data.distributed.DistributedSampler(train_dataset)
+        val_sampler = utils.data.distributed.DistributedSampler(val_dataset)
+
+        train_data_loader = utils.data.DataLoader(
+            dataset=train_dataset,
+            batch_size=config.local_batch_size,
+            sampler=train_sampler,
+            num_workers=config.num_workers,
+            pin_memory=True
+        )
+        val_data_loader = utils.data.DataLoader(
+            dataset=val_dataset,
+            batch_size=config.local_batch_size,
+            sampler=val_sampler,
+            num_workers=config.num_workers,
+            pin_memory=True
+        )
+
+        for epoch in range(last_epoch + 1, config.num_epochs):
+
+            train_sampler.set_epoch(epoch)
+
+            model.train()
+
+            for local_step, (images, labels) in enumerate(train_data_loader):
+
+                step_begin = time.time()
+
+                images = images.cuda(non_blocking=True)
+                labels = labels.cuda(non_blocking=True)
+
+                logits = model(images)
+                loss = criterion(logits, labels)
+
+                network_optimizer.zero_grad()
+
+                loss.backward()
+
+                # NOTE: Rethink this design.
+                average_gradients(model.network.parameters())
+                network_optimizer.step()
+
+                predictions = torch.argmax(logits, dim=1)
+                accuracy = torch.mean((predictions == labels).float())
+
+                average_tensors([loss, accuracy])
+
+                step_end = time.time()
+
+                if config.global_rank == 0:
+                    summary_writer.add_scalars(
+                        main_tag='loss',
+                        tag_scalar_dict=dict(train=loss),
+                        global_step=global_step
+                    )
+                    summary_writer.add_scalars(
+                        main_tag='accuracy',
+                        tag_scalar_dict=dict(train=accuracy),
+                        global_step=global_step
+                    )
+                    print(f'[training] epoch: {epoch} global_step: {global_step} local_step: {local_step} '
+                          f'loss: {loss:.4f} accuracy: {accuracy:.4f} [{step_end - step_begin:.4f}s]')
+
+                global_step += 1
+
+            if config.global_rank == 0:
+
+                torch.save(dict(
+                    network_state_dict=model.network.state_dict(),
+                    architecture_state_dict=model.architecture.state_dict(),
+                    network_optimizer_state_dict=network_optimizer.state_dict(),
+                    last_epoch=epoch,
+                    global_step=global_step
+                ), f'{config.checkpoint_directory}/epoch_{epoch}')
+
+            lr_scheduler.step()
+
+            if config.validation:
+
+                model.eval()
+
+                with torch.no_grad():
+
+                    average_loss = 0
+                    average_accuracy = 0
+
+                    for local_step, (images, labels) in enumerate(val_data_loader):
+
+                        images = images.cuda(non_blocking=True)
+                        labels = labels.cuda(non_blocking=True)
+
+                        logits = model(images)
+                        loss = criterion(logits, labels)
+
+                        predictions = torch.argmax(logits, dim=1)
+                        accuracy = torch.mean((predictions == labels).float())
+
+                        average_tensors([loss, accuracy])
+
+                        average_loss += loss
+                        average_accuracy += accuracy
+
+                    average_loss = average_loss / (local_step + 1)
+                    average_accuracy = average_accuracy / (local_step + 1)
+
+                if config.global_rank == 0:
+                    summary_writer.add_scalars(
+                        main_tag='loss',
+                        tag_scalar_dict=dict(val=average_loss),
+                        global_step=global_step
+                    )
+                    summary_writer.add_scalars(
+                        main_tag='accuracy',
+                        tag_scalar_dict=dict(val=average_accuracy),
+                        global_step=global_step
+                    )
+                    print(f'[validation] epoch: {epoch} loss: {average_loss:.4f} accuracy: {average_accuracy:.4f}')
+
+    if config.global_rank == 0:
+        summary_writer.close()
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(description='DARTS: Differentiable Architecture Search')
+    parser.add_argument('--config', type=str, default='config.json')
+    parser.add_argument('--checkpoint', type=str, default='')
+    parser.add_argument('--training', action='store_true')
+    parser.add_argument('--validation', action='store_true')
+    parser.add_argument('--evaluation', action='store_true')
+    parser.add_argument('--inference', action='store_true')
+    parser.add_argument('--local_rank', type=int)
+    args = parser.parse_args()
+
+    main(args)
